5f482bb49f52df08df2f3a5809361c7aa438d69f
sanya6456 <66361881+sanya6456@users.noreply.github.com>
2020-06-09 14:52:20 +0200

    update05 finished (responsive too)

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-
+    $('.openmenubg').hide();
+    $('.menu').click(function(){
+        $('.openmenubg').slideDown(500);
+        $('.grid').hide();
+        $('.header').delay(500).queue(function(next){
+            $(this).css('background-image','none');
+            next();
+        });
+    });
+    $('.fa-times').click(function(){
+        $('.openmenubg').slideUp(500);
+        $('.grid').delay(500).fadeIn(200);
+        $('.header').css('background-image','linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url(gallery/womenbg011.jpg)');
+    });
 });
